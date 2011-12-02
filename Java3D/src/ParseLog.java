302f505393e750dbff2a0807c07f47f4f12e68a2
Ryan Kane <rmkane89@gmail.com>
2011-12-01 21:23:22 -0500

    Parser added

---
+++ Java3D/src/ParseLog.java
@@ -0,0 +1,87 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ParseLog {
+	// COMMANDS:
+	// Move, Rotate, Scale, Resize, Zoom
+	private final String create = "cre", move = "mov", rotate = "rot",
+			scale = "scl", resize = "rsz", zoom = "zom";
+
+	// SHAPES (Solids):
+	// Prisms: Rectangular, Triangular, & Hexagonal
+	// Pyramids: Square & Rectangular
+	// Cylinder
+	// Sphere
+	// Cone - We don't support this YET...
+	private final String rectangle = "rec", triangle = "tri", hexagon = "hex",
+			pyramid = "pyr", cylinder = "cyl", sphere = "sph";
+
+	public ParseLog() {
+
+		String selected_file = "";
+		File file = null;
+		Scanner scan = null;
+
+		try {
+			selected_file = "2011_12_01_19_12_46.log";
+			file = new File(selected_file);
+			scan = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("File not found!");
+		}
+
+		while (scan.hasNextLine()) {
+			String line = scan.nextLine();
+			String[] seg = line.split(";");
+
+			// For all lines
+			String cmd = seg[0]; // Command
+
+			if (cmd.equals(zoom)) {
+				double percent = Double.parseDouble(seg[1]); // Percentage
+				System.out.printf("You zoomed in %.2f%%.\n", percent);
+			} else {
+				String shp = seg[1]; // Action
+				if (cmd.equals(create)) {
+					System.out.printf("You created a %s.\n", shp);
+				} else if (cmd.equals(move)) {
+					double x = Double.parseDouble(seg[2]); // X-Axis
+															// Translation
+					double y = Double.parseDouble(seg[3]); // Y-Axis
+															// Translation
+					double z = Double.parseDouble(seg[4]); // Z-Axis
+															// Translation
+					System.out.printf("You moved a %s (%.2f, %.2f, %.2f).\n",
+							shp, x, y, z);
+				} else if (cmd.equals(rotate)) {
+					String axis = seg[2]; // Which axis?
+					int numRot = Integer.parseInt(seg[3]);
+					System.out
+							.printf("You rotated a %s %d times around the %s-axis.\n",
+									shp, numRot, axis);
+				} else if (cmd.equals(scale)) {
+					double percent = Double.parseDouble(seg[2]); // Percentage
+					System.out
+							.printf("You scaled a %s %.2f%%.\n", shp, percent);
+				} else if (cmd.equals(resize)) {
+					double height = Double.parseDouble(seg[2]); // X-Axis
+																// Translation
+					double width = Double.parseDouble(seg[3]); // Y-Axis
+																// Translation
+					double depth = Double.parseDouble(seg[4]); // Z-Axis
+																// Translation
+					System.out.printf(
+							"You resized a %s to %.2f&x%.2f&x%.2f&.\n", shp);
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Program Opened");
+		new ParseLog();
+		System.out.println("Program Closed");
+	}
+}
